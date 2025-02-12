01c4fc966bc709ce5249b67067df8ca596e9035a
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-02-12 18:33:34 +0100

    Added Root Layout and Constants

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const Homepage = () => {
+	return (
+		<>
+			<Button>Click me!</Button>
+		</>
+	);
+};
+
+export default Homepage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,35 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/assets/styles/globals.css";
+import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Prostore",
-  description: "A modern ecommerce platform built with Next.js",
+	title: {
+		template: `%s | Prostore`,
+		default: APP_NAME,
+	},
+	description: APP_DESCRIPTION,
+	metadataBase: new URL(SERVER_URL),
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+		</html>
+	);
 }
