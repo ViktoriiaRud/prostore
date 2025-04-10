931f9998b2017610e1f1f41295ba200a7f176cc9
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-04-10 15:29:29 +0200

    Bug fix component ModeToggle

--- components/shared/header/mode-toggle.tsx
+++ components/shared/header/mode-toggle.tsx
@@ -1,67 +1,68 @@
 "use client";
-import { useState, useEffect } from 'react';
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, 
-         DropdownMenuTrigger, 
-         DropdownMenuLabel, 
-         DropdownMenuSeparator, 
-         DropdownMenuContent,
-         DropdownMenuCheckboxItem, 
-        } from "@/components/ui/dropdown-menu";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon, SunMoon } from "lucide-react";
 import { useTheme } from "next-themes";
-import { SunIcon, MoonIcon, SunMoon } from "lucide-react";
+import { useEffect, useState } from 'react';
 
 const ModeToggle = () => {
-    const [ mounted, setMounted ] = useState(false);
-	const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+    const { theme, setTheme } = useTheme();
 
     useEffect(() => {
         setMounted(true)
     }, []);
 
-    if (!setMounted) {
+    if (!mounted) {
         return null;
     }
 
-	return (
-		<>
-			<DropdownMenu>
+    return (
+        <>
+            <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button
-                  variant="ghost"
-                  className="focus-visible:ring-0 focus-visible:ring-offset-0">
-                    {theme === 'system' ? (
-                        <SunMoon/>
-                    ) : theme === 'dark' ? (
-                        <MoonIcon/>
-                    ) : <SunIcon/>}
-                  </Button>
+                    <Button
+                        variant="ghost"
+                        className="focus-visible:ring-0 focus-visible:ring-offset-0">
+                        {theme === 'system' ? (
+                            <SunMoon />
+                        ) : theme === 'dark' ? (
+                            <MoonIcon />
+                        ) : <SunIcon />}
+                    </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
                     <DropdownMenuLabel>Appearance</DropdownMenuLabel>
-                    <DropdownMenuSeparator/>
+                    <DropdownMenuSeparator />
                     <DropdownMenuCheckboxItem
-                    checked={ theme === 'system'}
-                    onClick={ () => setTheme('system')}
+                        checked={theme === 'system'}
+                        onClick={() => setTheme('system')}
                     >
                         System
                     </DropdownMenuCheckboxItem>
                     <DropdownMenuCheckboxItem
-                    checked={ theme === 'dark'}
-                    onClick={ () => setTheme('dark')}
+                        checked={theme === 'dark'}
+                        onClick={() => setTheme('dark')}
                     >
                         Dark
                     </DropdownMenuCheckboxItem>
                     <DropdownMenuCheckboxItem
-                    checked={ theme === 'light'}
-                    onClick={ () => setTheme('light')}
+                        checked={theme === 'light'}
+                        onClick={() => setTheme('light')}
                     >
                         Light
                     </DropdownMenuCheckboxItem>
                 </DropdownMenuContent>
             </DropdownMenu>
-		</>
-	);
+        </>
+    );
 };
 
 export default ModeToggle;
