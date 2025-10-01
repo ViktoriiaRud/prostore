c27ca739f787fb457c5929a52750ec0fb97e21c0
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-10-01 11:56:17 +0200

    Added component CartTable

---
+++ app/(root)/cart/cart-table.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { useToast } from '@/hooks/use-toast';
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+
+
+import { Cart } from "@/types";
+
+const CartTable = ({ cart }: { cart?: Cart }) => {
+    const router = useRouter();
+    const { toast } = useToast();
+    const [isPending, startTransition] = useTransition();
+
+    return <>
+        <h1 className="py-4 h2-bold">Shopping Cart</h1>
+        {!cart || cart.items.length === 0 ? (
+            <div>
+                Cart is empty. <Link href='/'>go Shopping</Link>
+            </div>
+        ) : (
+            <div className='grid md:grid-cols-4 md:gap-5'>
+                <div className='overflow-x-auto md:col-span-3'>Table</div>
+            </div>
+        )}
+    </>;
+}
+
+export default CartTable;
