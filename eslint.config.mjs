64bae0cbdd37c0b98467f843e732515a77ba1284
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-10-13 09:08:29 +0200

    fix- eslint.config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import { dirname } from "path";
-import tseslint from "typescript-eslint";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginTs from '@typescript-eslint/eslint-plugin';
+import parser from '@typescript-eslint/parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,12 +12,23 @@ const compat = new FlatCompat({
 });
 
 export default [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
 
-  ...tseslint.config({
-    rules: {
-      "no-unused-vars": "off",
-      "@typescript-eslint/no-unused-vars": "error",
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
     },
-  }),
+    plugins: {
+      '@typescript-eslint': eslintPluginTs,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['error'],
+    },
+  },
 ];
+
