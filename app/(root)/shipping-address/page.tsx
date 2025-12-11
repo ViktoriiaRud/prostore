9ec91febc3e41724f58c563a58b76657e58eacb9
Viktoriia Rudneva <viktoriiadeveloper@gmail.com>
2025-12-11 13:17:46 +0100

    Added Order Zod Schemas and Types

--- app/(root)/shipping-address/page.tsx
+++ app/(root)/shipping-address/page.tsx
@@ -2,7 +2,7 @@ import { auth } from "@/auth";
 import CheckoutSteps from "@/components/shared/checkout-steps";
 import { getMyCart } from "@/lib/actions/cart.actions";
 import { getUserById } from "@/lib/actions/user.actions";
-import { shippingAddress } from "@/types";
+import { ShippingAddress } from "@/types";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import ShippingAddressForm from "./shipping-address-form";
@@ -29,7 +29,7 @@ const ShippingAddressPage = async () => {
 	return (
 		<>
 			<CheckoutSteps current={1} />
-			<ShippingAddressForm address={user.address as shippingAddress} />
+			<ShippingAddressForm address={user.address as ShippingAddress} />
 		</>
 	);
 };
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-import { cartItemSchema, insertCartSchema, insertProductSchema, shippingAddressSchema } from "@/lib/validators";
+import { cartItemSchema, insertCartSchema, insertOrderItemSchema, insertOrderSchema, insertProductSchema, shippingAddressSchema } from "@/lib/validators";
 import { z } from 'zod';
 
 export type Product = z.infer<typeof insertProductSchema> & {
@@ -9,4 +9,13 @@ export type Product = z.infer<typeof insertProductSchema> & {
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
-export type shippingAddress = z.infer<typeof shippingAddressSchema>;
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+    id: string;
+    createAt: Date;
+    isPaid: Boolean;
+    paidAt: Date | null;
+    orderItems: OrderItem[];
+    user: {name: string; email: string};
+};
